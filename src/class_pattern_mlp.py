11b19800e27d66da4ef69ef8bec1bd493dae6057
Jack Laurenson <jack.laurenson@zalando.de>
2023-02-19 23:53:42 +0100

    Converted to manually computing the last part of the cross entropy loss using one hot encoded class labels. We can use this same approach when we introduce variance minimization

--- src/class_pattern_mlp.py
+++ src/class_pattern_mlp.py
@@ -4,7 +4,7 @@ from torch import nn
 from src import dataset_creator, splitter
 
 # Set the seed for reproduceability
-# torch.manual_seed(59283)
+torch.manual_seed(59284)
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Fix issues with ssl certificates
@@ -15,7 +15,7 @@ batch_size = 40  # The batch size when training or evaluating the network
 total_n = 160
 num_classes = 8
 noisy_d = 20
-percent_correct = 0.8
+percent_correct = 1.0
 
 binary_pattern_dataset = dataset_creator.class_pattern_with_noise(total_n, num_classes, noisy_d, percent_correct, 1)
 train_loader, val_loader = splitter.train_val_split(binary_pattern_dataset, batch_size, m)
@@ -45,13 +45,12 @@ mlp = Mlp(num_input, num_hidden, num_classes, m)
 
 # Loss and optimizer
 softmax = nn.Softmax(dim=1)
-bias = nn.CrossEntropyLoss()
-variance = nn.CrossEntropyLoss()
+log_softmax = nn.LogSoftmax(dim=1)
 optimizer = torch.optim.SGD(mlp.parameters(), lr=learning_rate, momentum = momentum)
 
 def eval_train_accuracy(y_train, label):
     _, predicted = torch.max(y_train, 2)
-    total = label.size(0) * label.size(1)
+    total = y_train.size(0) * y_train.size(1)
     correct = (predicted == label).sum().item()
     return correct / total
 
@@ -77,26 +76,28 @@ def eval_accuracy(data_loader, model):
 for epoch in range(num_epochs):
     print('Epoch {}'.format(epoch))
     for image, label, train_with, train_without in train_loader:
+        current_batch_size = len(label)
+        indices_for_batch = torch.arange(current_batch_size, dtype=torch.int64)
+        one_hot = torch.zeros(current_batch_size, num_classes)
+        one_hot[indices_for_batch, label] = 1.0
         y_hats = []
-        labels = []
         for j in range(m):
             y_hats.append(mlp(image, j))
-            labels.append(label)
-        labels = torch.stack(labels)
         y_hat = torch.stack(y_hats) # Shape (m, batch_size, num_classes)
         # Separate into the models to be trained, and those to be used as variance minimizing means
         model_indices = torch.transpose(train_with, 0, 1)
-        indices_for_batch = torch.arange(len(label), dtype=torch.int64)
+
         y_hat_train = y_hat[model_indices, indices_for_batch] # Shape (m // 2, batch_size, num_classes)
-        label_train = labels[model_indices, indices_for_batch] # Shape (m // 2, batch_size, num_classes)
         loss = 0.0
         for j in range(m // 2):
-            loss += bias(y_hat_train[j], label_train[j])
+            ls = log_softmax(y_hat_train[j])
+            loss_per_example = one_hot * ls # + (1 - one_hot) * (1 - ls)
+            loss -= torch.mean(torch.sum(loss_per_example, axis=1))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         # print(loss.item())
-        print('Train Acc: {}'.format(eval_train_accuracy(y_hat_train, label_train)))
+        print('Train Acc: {}'.format(eval_train_accuracy(y_hat_train, label)))
     print('Val Acc: {}'.format(eval_accuracy(val_loader, mlp)))
     print('')
         # y_hat_without = y_hat[train_without]  # Shape (m // 2, batch_size, num_classes)
