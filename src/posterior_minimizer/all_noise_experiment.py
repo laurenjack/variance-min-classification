62c23eaefd71d09df58eae8244cfa4d195ba5601
Jack Laurenson <jack.laurenson@zalando.de>
2024-07-05 13:54:58 +0200

    Adding multilayer support, different posterior constants

--- src/posterior_minimizer/all_noise_experiment.py
+++ src/posterior_minimizer/all_noise_experiment.py
@@ -6,21 +6,22 @@ from src import hyper_parameters
 from src import dataset_creator
 from src.posterior_minimizer import runner
 
-torch.manual_seed(56433)
+# torch.manual_seed(56433)
 
 runs = 100
 n = 100
 n_test = 100
-d = 1
+d = 20
 desired_success_rate = 0.728
 bp = (1 - desired_success_rate ** (1/d)) / 2
 print(bp)
 threshold_success = binom.ppf(1 - bp, n, 0.5) # - 1
 deviation = threshold_success - n * 0.5
 print(deviation)
-# post_constant = threshold_success / n - 0.5
+binom_post_constant = threshold_success / n - 0.5
 post_constant = norm.ppf(1 - bp, scale=0.5/n ** 0.5)
 print(f"Regularization Constant: {post_constant}")
+print(f"Binom Post Constant: {binom_post_constant}")
 
 hp = hyper_parameters.HyperParameters(batch_size=n,
                                       epochs=300,
@@ -29,12 +30,11 @@ hp = hyper_parameters.HyperParameters(batch_size=n,
                                       weight_decay=0.0,
                                       post_constant=post_constant,
                                       gamma=0.8,
-                                      single_moving=None,
                                       is_adam=True,
                                       all_linear=True,
                                       print_epoch=False,
                                       print_batch=False)
 
 problem = dataset_creator.AllNoise(num_class=2, d=d)
-runner.run(problem, runs, n, n_test, hp, 0, shuffle=True)
+runner.run(problem, runs, n, n_test, [d, 1], hp, 0, shuffle=True)
 
